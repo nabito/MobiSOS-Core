ab3c3b23dc6936e7180ff89f0674c4d7788e220d
nabito <nabito@gmail.com>
2013-12-26 02:03:16 +0900

    Refactor mobiSOS property to be static. Added support for Jena spatial query.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,32 @@
+import java.io.IOException;
+
 import com.dadfha.mobisos.MobiSosCore;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		MobiSosCore core = new MobiSosCore();		
+			
+		//Resource user = core.createUser("nabito@gmail.com", "mypwd");
+		//String uid = user.getProperty(MobiSosCore.PROP_UUID).getString();
 		
-		Resource user = core.createUser("nabito@gmail.com", "mypwd");
-		String uid = user.getProperty(core.PROP_UUID).getString();
+		//System.out.println(uid);
 		
-		System.out.println(uid);
 		
 		String titaLoc = "{'accuracy': 100,'altitude': 0,'altitudeAccuracy': null,'heading': 0,'latitude': 40.493781233333333,'longitude': -80.056671,'speed': 0,'timestamp': 1318426498331}";
 		// if MAC address is to be used as part of resource ID (local name) it must not have ':'
-		core.checkInWifi(uid, titaLoc, "aabbccddeeff");
-		core.checkInWifi(uid, titaLoc, "03a004d30011");
-		//core.checkInAtWifi("3102201656413", titaLoc, "aa:bb:cc:dd:ee:ff");
+		//core.checkInWifi(uid, titaLoc, "aabbccddeeff");
+		//core.checkInWifi(uid, titaLoc, "03a004d30011");
+		
+		//core.addSamples();
+		
+		core.sosCall("3012201656413", titaLoc);
 		
 		core.dumpTDB();
+		
 		
 		System.out.println("Main class is intended for testing purpose only. Statements will be written to TDB and dump.");
 
--- src/com/dadfha/mobisos/MobiSosCore.java
+++ src/com/dadfha/mobisos/MobiSosCore.java
@@ -1,67 +1,121 @@
 package com.dadfha.mobisos;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
+import org.apache.jena.atlas.lib.StrUtils;
+import org.apache.jena.atlas.logging.Log;
 import org.apache.jena.fuseki.Fuseki;
 import org.apache.jena.fuseki.FusekiCmd;
 
 import com.google.gson.Gson;
 import com.hp.hpl.jena.query.ARQ;
 import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Seq;
+import com.hp.hpl.jena.sparql.util.QueryExecUtils;
 import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 import com.hp.hpl.jena.tdb.TDB;
 import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.vocabulary.RDFS;
+
+import org.apache.jena.query.spatial.EntityDefinition;
+import org.apache.jena.query.spatial.SpatialDatasetFactory;
+import org.apache.jena.query.spatial.SpatialQuery;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MobiSosCore {
 	
-	private static final String TDB_DIR = "/Users/Wirawit/dev/jena-fuseki-1.0.0/MobiSosTDB";
+	static {
+		Log.setLog4j();
+	}
+	static Logger log = LoggerFactory.getLogger("MobiSosCoreLogger");	
 	
-	//@prefix n: <http://dadfha.com/ns/node#> .
-	private static final String NS_MOBISOS = "http://www.dadfha.com/mobisos-rdf/1.0#";
+	private static final String TDB_PATH = "/Users/Wirawit/dev/jena-fuseki-1.0.0/MobiSosTDB";
+	private static final String TDB_INDEX_PATH = "/Users/Wirawit/dev/jena-fuseki-1.0.0/MobiSosTdbIndex";
+	private static final File TDB_DIR = new File(TDB_PATH);
+	private static final File TDB_INDEX_DIR = new File(TDB_INDEX_PATH);
+	
+	// IMP Put URIs and Prefixes in a map collection for auto population  
+	protected static final String URI_BASE = "http://www.dadfha.com/mobisos-rdf/1.0#";
+	private static final String URI_W3C_GEO = "http://www.w3.org/2003/01/geo/wgs84_pos#";
+	private static final String URI_JENA_SPATIAL = "http://jena.apache.org/spatial#";
+	
 	// Prefix must be a valid NCName - http://en.wikipedia.org/wiki/QName
-	private static final String NS_PREFIX_MOBISOS = "mbs";
+	private static final String URI_PREFIX_BASE = "mbs";
+	private static final String URI_PREFIX_RDFS = "rdfs";
+	private static final String URI_PREFIX_W3C_GEO = "geo";
+	private static final String URI_PREFIX_JENA_SPATIAL = "spatial";
+	
 	// Resource naming prefix
 	private static final String RES_PREFIX_USER = "user"; // for user	
 	private static final String RES_PREFIX_REC = "records"; // for records
 	private static final String RES_PREFIX_CHK = "chk"; // for check-in
 	private static final String RES_PREFIX_WIFI = "wifi"; // for wifi
 	
-	
-	private final Dataset dataset = TDBFactory.createDataset(TDB_DIR);
+	private Dataset dataset;
 	private Model model;
 	
-	public final Property PROP_UUID;
-	public final Property PROP_HAS_CHECKIN;
-	public final Property PROP_VIA;
-	public final Property PROP_TIMESTAMP;
-	public final Property PROP_GEO_LOC;
-	public final Property PROP_UCODE;
+	//public static final Resource RES_XXX  = resource( "Xxx");	
+	
+	// IMP consider adding rdfs:label and rdfs:comment for each of property
+	// so it's become sentences and can be saved to TDB
+	public static final Property PROP_UUID = property("uuid");
+	public static final Property PROP_HAS_CHECKIN = property("hasCheckin");
+	public static final Property PROP_VIA = property("via");
+	public static final Property PROP_TIMESTAMP = property("timestamp");
+	public static final Property PROP_GEO_LOC = property("geolocation");
+	public static final Property PROP_UCODE = property("ucode");
 
-	public final Property PROP_MAC_ADDR;
-	public final Property PROP_LAST_UPDATE;
-	public final Property PROP_LAST_SEEN;
-	public final Property PROP_L10N_METHOD;
+	public static final Property PROP_MAC_ADDR = property("macAddress");
+	public static final Property PROP_LAST_UPDATE = property("lastUpdate");
+	public static final Property PROP_LAST_SEEN = property("lastSeen");
+	public static final Property PROP_L10N_METHOD = property("l10nMethod");
 	
 	// GeoLocation related properties
-	public final Property PROP_ACCU;
-	public final Property PROP_ALTI;
-	public final Property PROP_ALTI_ACCU;
-	public final Property PROP_HEADING;	
-	public final Property PROP_LAT;
-	public final Property PROP_LONG;
-	public final Property PROP_SPEED;
-	public final Property PROP_LOC_NAME;		
+	public static final Property PROP_ACCU = property("accuracy");
+	public static final Property PROP_ALTI = property("altitude");
+	public static final Property PROP_ALTI_ACCU = property("altitudeAccuracy");
+	public static final Property PROP_HEADING = property("heading");
+	public static final Property PROP_LAT = property("latitude");
+	public static final Property PROP_LONG = property("longitude");	
+	public static final Property PROP_SPEED = property("speed");
+	public static final Property PROP_LOC_NAME = property("locationName");
 	
-	private static Gson gson = new Gson();
-			
-	public MobiSosCore() {
+	public static final Property W3C_LAT = property(URI_W3C_GEO, "lat");
+	public static final Property W3C_LONG = property(URI_W3C_GEO, "long");
+	
+	private static final Gson gson = new Gson();
+	
+    protected static final Resource resource( String local )
+    { return ResourceFactory.createResource( URI_BASE + local ); }
+    
+    protected static final Resource resource( String uri, String local )
+    { return ResourceFactory.createResource( uri + local ); }	    
+	
+    protected static final Property property( String local )
+    { return ResourceFactory.createProperty( URI_BASE, local ); }
+    
+    protected static final Property property( String uri, String local )
+    { return ResourceFactory.createProperty( uri, local ); }	    
+		
+	public MobiSosCore() throws IOException {
 				
+		// Fuseki start-up
 		new Thread(new Runnable(){
 			@Override
 			public void run() {
@@ -80,15 +134,19 @@ public class MobiSosCore {
 			}
 			
 		}).start();
+		
+		dataset = initTDBDatasetWithLuceneSpatitalIndex(TDB_INDEX_DIR, TDB_DIR, true);
 
+		//loadData(geoDataset, "/Users/Wirawit/Desktop/geoarq-data-1.ttl");		
 		
 		// Init TDB (even if when already done before)				
 		dataset.begin(ReadWrite.WRITE);
 		model = dataset.getDefaultModel();
 		// IMP consider naming graph for better management/performance?
 		// dataset.addNamedModel(NS_MOBISOS + NS_PREFIX_MOBISOS, model);
-		model.setNsPrefix(NS_PREFIX_MOBISOS, NS_MOBISOS);
-		
+		model.setNsPrefix(URI_PREFIX_BASE, URI_BASE);
+		model.setNsPrefix(URI_PREFIX_W3C_GEO, URI_W3C_GEO);
+				
 		/*
 		//model = ModelFactory.createDefaultModel();
 		Resource r = model.createResource(NS_MOBISOS + "root");
@@ -100,47 +158,29 @@ public class MobiSosCore {
 		model.add(p2, RDFS.label, "The Vaja");
 		//model.write(System.out, "Turtle");
 		 */
-		
-		// IMP consider adding rdfs:label and rdfs:comment for each of property
-		// so it's become sentences and can be saved to TDB
-		PROP_UUID = model.createProperty(NS_MOBISOS, "uuid");
-		PROP_HAS_CHECKIN = model.createProperty(NS_MOBISOS, "hasCheckin");
-		PROP_VIA = model.createProperty(NS_MOBISOS, "via");
-		PROP_TIMESTAMP = model.createProperty(NS_MOBISOS, "timestamp");
-		PROP_GEO_LOC = model.createProperty(NS_MOBISOS, "geoLocation");		
-		PROP_UCODE = model.createProperty(NS_MOBISOS, "ucode");
-		
-		PROP_MAC_ADDR = model.createProperty(NS_MOBISOS, "macAddress");
-		PROP_LAST_UPDATE = model.createProperty(NS_MOBISOS, "lastUpdate");
-		PROP_LAST_SEEN = model.createProperty(NS_MOBISOS, "lastSeen");		
-		PROP_L10N_METHOD = model.createProperty(NS_MOBISOS, "l10nMethod");
-		
-		PROP_ACCU = model.createProperty(NS_MOBISOS, "accuracy");
-		PROP_ALTI = model.createProperty(NS_MOBISOS, "altitude");
-		PROP_ALTI_ACCU = model.createProperty(NS_MOBISOS, "altitudeAccuracy");
-		PROP_HEADING = model.createProperty(NS_MOBISOS, "heading");
-		PROP_LAT = model.createProperty(NS_MOBISOS, "latitude");
-		PROP_LONG = model.createProperty(NS_MOBISOS, "longitude");		
-		PROP_SPEED = model.createProperty(NS_MOBISOS, "speed");
-		PROP_LOC_NAME = model.createProperty(NS_MOBISOS, "locationName");
-				
+
+		model.close();		
 		dataset.commit();
-		model.close();
 		dataset.end();
 		
 		
 	}
 	
-	public void sosCall(String uid, String location) {
-		// TODO update latest location to DB and start real-time tracking mode
-		//System.out.println("DUH?");
+	public void sosCall(String uid, String location) throws IOException {
 		
-		boolean success = false;
+		System.out.println("SOS called on mobiSOS-Core");
 		
+		boolean success = true;
+		
+		// TODO update latest checkin to DB, and turn user's SOS flag on
+		
+		
+		// query semantic DB for nearby nodes
+		queryData(dataset);
 		
 		if(!success) throw new RuntimeException("Can't complete sos call operation.");
 		
-		// query semantic DB for nearby nodes
+		
 		// then ask for tracking record "Do you see Bob?"		
 	}
 	
@@ -176,7 +216,7 @@ public class MobiSosCore {
 		// Note: ordered index is not used/generated here due to possible racing condition
 		// which could break the true order of event, rather we rely on SEQ collection order	
 		
-		Resource user = model.createResource(NS_MOBISOS + RES_PREFIX_USER + uid);
+		Resource user = model.createResource(URI_BASE + RES_PREFIX_USER + uid);
 		
 		// Only allow authorized user to check-in
 		//if(!model.listResourcesWithProperty(PROP_UUID, uid).hasNext()) {
@@ -193,7 +233,7 @@ public class MobiSosCore {
 		if(chkRecords == null) throw new RuntimeException("Possibly Data Error! This user still has no check-in record yet!");
 		
 		// This will also create this wifi resource if not available before (Wardriving like)
-		Resource wifi = model.createResource(NS_MOBISOS + RES_PREFIX_WIFI + macAddr);
+		Resource wifi = model.createResource(URI_BASE + RES_PREFIX_WIFI + macAddr);
 
 		// Extract location's parameters from JSON and put it in bnode
 		GeoLocation geoLoc = new GeoLocation(gson.fromJson(location, TitaniumLocation.class));
@@ -207,15 +247,15 @@ public class MobiSosCore {
 				.addLiteral(PROP_SPEED, geoLoc.getSpeed())
 				.addLiteral(PROP_LOC_NAME, ""); // IMP can we get location name from wifi check-in in the future?
 		
-		Resource checkin = model.createResource(NS_MOBISOS + RES_PREFIX_CHK + generateUUID())
+		Resource checkin = model.createResource(URI_BASE + RES_PREFIX_CHK + generateUUID())
 				.addProperty(PROP_VIA, wifi)
 				.addLiteral(PROP_TIMESTAMP, System.currentTimeMillis())
 				.addProperty(PROP_GEO_LOC, locBnode);
 		
 		chkRecords.add(checkin);		
 		
-		dataset.commit();
 		model.close();
+		dataset.commit();
 		dataset.end();
 		
 		return true;
@@ -262,23 +302,23 @@ public class MobiSosCore {
 	 * 
 	 * @param email
 	 * @param passwd
-	 * @return
+	 * @return String unique user id
 	 */
-	public Resource createUser(String email, String passwd) {
+	public String createUser(String email, String passwd) {
 		// TODO include password someday soon?
 		dataset.begin(ReadWrite.WRITE);
 		model = dataset.getDefaultModel();
 		
 		String uid = generateUUID();
-		Resource user = model.createResource(NS_MOBISOS + RES_PREFIX_USER + uid)
-				.addProperty(PROP_HAS_CHECKIN, model.createSeq(NS_MOBISOS + RES_PREFIX_REC + uid))
+		Resource user = model.createResource(URI_BASE + RES_PREFIX_USER + uid)
+				.addProperty(PROP_HAS_CHECKIN, model.createSeq(URI_BASE + RES_PREFIX_REC + uid))
 				.addProperty(FOAF.mbox, email)
 				.addProperty(PROP_UUID, uid);	
 		
-		dataset.commit();
 		model.close();
+		dataset.commit();
 		dataset.end();
-		return user;		
+		return uid;		
 	}
 	
 	public String getUserById(String uid) {
@@ -337,6 +377,219 @@ public class MobiSosCore {
 		model.close();
 		dataset.end();
 	}
+	
+
+    private static Dataset initTDBDatasetWithLuceneSpatitalIndex(File indexDir, File TDBDir, boolean preserveTdbData) throws IOException{
+		SpatialQuery.init();
+		deleteOldFiles(indexDir);
+		indexDir.mkdirs();
+		if(!preserveTdbData) {
+			deleteOldFiles(TDBDir);
+			TDBDir.mkdir();			
+		}
+		return createDatasetByCode(indexDir, TDBDir);
+    }	
+    
+    /**
+     * deleteOldFiles
+     * @param indexDir
+     */
+	private static void deleteOldFiles(File indexDir) {
+		if (indexDir.exists())
+			emptyAndDeleteDirectory(indexDir);
+	}    
+	
+	/**
+	 * emptyAndDeleteDirectory
+	 * @param dir
+	 */
+    private static void emptyAndDeleteDirectory(File dir) {
+        File[] contents = dir.listFiles() ;
+        if (contents != null) {
+            for (File content : contents) {
+                if (content.isDirectory()) {
+                    emptyAndDeleteDirectory(content) ;
+                } else {
+                    content.delete() ;
+                }
+            }
+        }
+        dir.delete() ;
+    }	
+    
+    /**
+     * createDatasetByCode
+     * @param indexDir
+     * @param TDBDir
+     * @return
+     * @throws IOException
+     */
+	private static Dataset createDatasetByCode(File indexDir, File TDBDir) throws IOException {
+		// Base data
+		Dataset ds1 = TDBFactory.createDataset(TDBDir.getAbsolutePath());
+		return joinIndexDataset(indexDir, ds1);
+	}    
+	
+	/**
+	 * joinIndexDataset
+	 * @param indexDir
+	 * @param baseDataset
+	 * @return
+	 * @throws IOException
+	 */
+	private static Dataset joinIndexDataset(File indexDir, Dataset baseDataset) throws IOException{
+		EntityDefinition entDef = new EntityDefinition("entityField", "geoField");
+	
+		// set custom geo predicates
+		entDef.addSpatialPredicatePair(PROP_LAT, PROP_LONG);
+		entDef.addWKTPredicate(ResourceFactory.createResource("http://localhost/jena_example/#wkt_1"));
+		
+		// Lucene, index in File system.
+		Directory dir = FSDirectory.open(indexDir);
+
+		// Join together into a dataset
+		Dataset ds = SpatialDatasetFactory.createLucene(baseDataset, dir, entDef);
+
+		return ds;
+	}
+	
+	/**
+	 * Load RDF data from file to TDB 
+	 * @param ds
+	 * @param file
+	 */
+	public static void loadData(Dataset ds, String file) {
+		log.info("Start loading");
+		long startTime = System.nanoTime();
+		ds.begin(ReadWrite.WRITE);
+		try {
+			Model m = ds.getDefaultModel();
+			RDFDataMgr.read(m, file);
+			ds.commit();
+		} finally {
+			ds.end();
+		}
+
+		long finishTime = System.nanoTime();
+		double time = (finishTime - startTime) / 1.0e6;
+		log.info(String.format("Finish loading - %.2fms", time));
+	}	
+	
+	private static void queryData(Dataset geoDs) throws IOException {
+		
+		long startTime;
+		long finishTime;
+		double time;
+		
+		String pre = StrUtils.strjoinNL("PREFIX " + URI_PREFIX_BASE + ": <" + URI_BASE + ">",
+				"PREFIX " + URI_PREFIX_JENA_SPATIAL + ": <" + URI_JENA_SPATIAL + ">",
+				"PREFIX " + URI_PREFIX_RDFS + ": <"+ RDFS.getURI() +">");
+		
+		/*
+		String precheck = StrUtils.strjoinNL("PREFIX mbs: <http://www.dadfha.com/mobisos-rdf/1.0#>",
+				"PREFIX spatial: <http://jena.apache.org/spatial#>",
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>");		
+		
+		if(pre.equals(precheck)) System.out.println("Yay"); else System.out.println("Boo"); 
+		*/
+		
+		String qs = "";
+		
+		System.out.println("withinCircle");
+		startTime = System.nanoTime();
+		qs = StrUtils.strjoinNL("SELECT * ",
+				" { ?s spatial:withinCircle (37.78583 -122.40641 10.0 'km' 3) ."," }");
+
+		geoDs.begin(ReadWrite.READ);
+		try {
+			Query q = QueryFactory.create(pre + "\n" + qs);
+			QueryExecution qexec = QueryExecutionFactory.create(q, geoDs);
+			QueryExecUtils.executeQuery(q, qexec);
+		} finally {
+			geoDs.end();
+		}
+		finishTime = System.nanoTime();
+		time = (finishTime - startTime) / 1.0e6;
+		log.info(String.format("FINISH - %.2fms", time));
+		
+		System.out.println("withinBox");
+		startTime = System.nanoTime();
+		qs = StrUtils.strjoinNL("SELECT * ",
+				" { ?s spatial:withinBox (30.00 -130.00 40.00 -100.00) .", " }");
+//				" { ?s spatial:withinBox (30.00 -100.00 40.00 -130.00 -1) ;",
+//				"      rdfs:label ?label", " }");
+
+		geoDs.begin(ReadWrite.READ);
+		try {
+			Query q = QueryFactory.create(pre + "\n" + qs);
+			QueryExecution qexec = QueryExecutionFactory.create(q, geoDs);
+			QueryExecUtils.executeQuery(q, qexec);
+		} finally {
+			geoDs.end();
+		}
+		finishTime = System.nanoTime();
+		time = (finishTime - startTime) / 1.0e6;
+		log.info(String.format("FINISH - %.2fms", time));		
+		
+		System.out.println("nearby");
+		qs = StrUtils.strjoinNL("SELECT * ",
+				" { ?s spatial:nearby (51.3000 -2.71000 100.0 'miles') ;",
+				"      rdfs:label ?label", " }");
+ 
+		geoDs.begin(ReadWrite.READ);
+		try {
+			Query q = QueryFactory.create(pre + "\n" + qs);
+			QueryExecution qexec = QueryExecutionFactory.create(q, geoDs);
+			QueryExecUtils.executeQuery(q, qexec);
+		} finally {
+			geoDs.end();
+		}
+		finishTime = System.nanoTime();
+		time = (finishTime - startTime) / 1.0e6;
+		log.info(String.format("FINISH - %.2fms", time));		
+
+		
+		// dump query
+		/*
+		System.out.println("dump");
+		startTime = System.nanoTime();
+		qs = StrUtils.strjoinNL("SELECT * ",
+				" { ?s ?p ?o .", " }");
+
+		geoDs.begin(ReadWrite.READ);
+		try {
+			Query q = QueryFactory.create(pre + "\n" + qs);
+			QueryExecution qexec = QueryExecutionFactory.create(q, geoDs);
+			QueryExecUtils.executeQuery(q, qexec);
+		} finally {
+			geoDs.end();
+		}
+		finishTime = System.nanoTime();
+		time = (finishTime - startTime) / 1.0e6;
+		log.info(String.format("FINISH - %.2fms", time));	
+		*/		
+		
+		
+	}
+	/*
+	public void addSamples() {
+		
+		// Init TDB (even if when already done before)				
+		dataset.begin(ReadWrite.WRITE);
+		model = dataset.getDefaultModel();
+				
+		//model = ModelFactory.createDefaultModel();
+		Resource r = model.createResource(URI_BASE + "tokyo");
+
+		model.addLiteral(r, W3C_LAT, 37.78583f);
+		model.addLiteral(r, W3C_LONG, -122.40641f);	
+				
+		model.close();
+		dataset.commit();
+		dataset.end();
+	
+	}
+	*/
 	
 
 }
